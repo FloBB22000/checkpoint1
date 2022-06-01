94790f8cbf14c4fc6f0d56dde9ce7967c912ea0a
FloBB22000 <florence.goslino@sncf.fr>
2022-06-01 16:35:37 +0200

    ajout html, css, js

---
+++ site/js/portfolio.js
@@ -0,0 +1,12 @@
+const image = document.querySelector('.imgavatar1');
+
+image.addEventListener('click', function(){
+    image.src = "./image/logo-wcs.png";
+});
+
+const button1 = document.querySelector('.button1');
+const name = prompt("What's your name ?");
+
+button1.addEventListener('click', function(){
+   button1
+})
